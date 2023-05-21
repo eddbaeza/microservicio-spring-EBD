c49d8d7f7b9d14f32abd6115ebf1dfbdfa407f9a
eddbaeza <63975872+eddbaeza@users.noreply.github.com>
2023-05-21 18:06:47 -0400

    Update GreetingService.java

--- src/main/java/com/example/testingweb/GreetingService.java
+++ src/main/java/com/example/testingweb/GreetingService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class GreetingService {
+	private String password="1234";
 	public String greet() {
 		return "Finaliza el curso DevOps Foundation v4 - Eddie Baeza";
 	}
